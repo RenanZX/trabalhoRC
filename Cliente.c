4c40347b49f970cfcd4415552cd5d765a2bde6e5
Renan Godoi <renangodoimedeiros@gmail.com>
2017-06-26 04:54:03 +0100

    Update otimizado

--- Cliente.c
+++ Cliente.c
@@ -1,134 +1,66 @@
 #include <stdio.h>
-#ifdef __WIN32__
-#include <winsock2.h>
-#include <string.h>
- 
-#pragma comment(lib,"ws2_32.lib") /*Winsock Biblioteca*/
- 
-#define SERVER "127.0.0.1"  /*endereço IP do servidor UDP*/
-#define BUFLEN 512  /*tamanho maximo do buffer*/
-#define PORTA 8888   /*a porta que receberá os dados enviados do servidor*/
+#include <stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
 
-void StartCliente(){
-	struct sockaddr_in si_other;
-    int s, slen=sizeof(si_other);
-    char buf[BUFLEN];
-    char message[BUFLEN];
-    WSADATA wsa;
- 
-    //Inicializa o winsock
-    printf("\nInicializando Winsock...");
-    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-    {
-        printf("Falha. Codigo de Erro : %d",WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-    printf("Inicializado.\n");
-     
-    //cria o socket
-    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
-    {
-        printf("socket() falha com codigo de erro : %d" , WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-     
-    //estrutura do endereço
-    memset((char *) &si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORTA);
-    si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
+#define SERVER "127.0.0.1"
+#define BUFFTAM 1024  //Tamanho maximo do buffer
+#define PORTA 8888   //A porta na qual sera enviado os dados
 
-    //Faz o Bind
-    if( bind(s ,(struct sockaddr *)&si_other , sizeof(si_other)) == SOCKET_ERROR)
-    {
-        printf("Falha no bind com codigo de erro : %d" , WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-    puts("Bind feito");
-     
-    //começa a comunicacao com servidor
-    while(strcmp(buf,"fim")!=0)
-    {
-        printf("Digite uma mensagem : ");
-        gets(message);
-         
-        //envia a mensagem
-        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
-        {
-            printf("sendto() falhou codigo de erro : %d" , WSAGetLastError());
-            exit(EXIT_FAILURE);
-        }
-         
-        //recebe uma resposta e imprime na tela
-        //limpa o buffer
-        memset(buf,'\0', BUFLEN);
-        //tenta receber algum dado
-        if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
-        {
-            printf("recvfrom() falhou codigo de erro : %d" , WSAGetLastError());
-            exit(EXIT_FAILURE);
-        }
-         
-        puts(buf);
-    }
- 
-    closesocket(s);
-    WSACleanup();
-
+void terminar(char *s)
+{
+    perror(s);
+    exit(1);
 }
 
-#else
-#include <sys/socket.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+void IniciarCliente()
+{
+    struct sockaddr_in servidorsock;/*definindo as variaveis*/
+    int s, slen=sizeof(servidorsock);
+    char buf[BUFFTAM];
+    char mensagem[BUFFTAM];
 
-	void StartCliente(){
-		int clientSocket, nBytes;
-  		char buffer[1024];
-  		struct sockaddr_in serverAddr;
-  		socklen_t addr_size;
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+        terminar("socket");
+    }
 
-  		/*Cria um socket UDP*/
-  		clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+    memset((char *) &servidorsock, 0, sizeof(servidorsock));
+    servidorsock.sin_family = AF_INET;
+    servidorsock.sin_port = htons(PORTA);
 
-  		/*Configura a estrutura de endereço do servidor*/
-  		serverAddr.sin_family = AF_INET;
-  		serverAddr.sin_port = htons(7891);
-  		serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  		memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+    if (inet_aton(SERVER , &servidorsock.sin_addr) == 0)
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
 
-  		/*Faz o bind do socket com endereço da struct*/
-  		bind(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    while(strcmp(buf,"fim\n")!=0){
+        printf("Digite uma mensagem : ");
+        fgets(mensagem,BUFFTAM,stdin);
 
-  		/*Inicializa o tamanho da variavel que será ultilizada depois*/
-  		addr_size = sizeof serverAddr;
+        //Envia a mensagem
+        if (sendto(s, mensagem, strlen(mensagem) , 0 , (struct sockaddr *) &servidorsock, slen)==-1){
+            terminar("sendto()");
+        }
 
-  		while((strcmp(buffer,"FIM")!=0)&&(strcmp(buffer,"fim")!=0)){
-    		printf("Digite a mensagem que sera enviada ao servidor:\n");
-    		fgets(buffer,1024,stdin);
-    		printf("Voce digitou: %s",buffer);
+        //recebe uma mensagem de volta e printa ela na tela
+        //limpa o buffer de dados
+        memset(buf,'\0', BUFFTAM);
+        //tenta receber dados do servidor
+        if (recvfrom(s, buf, BUFFTAM, 0, (struct sockaddr *) &servidorsock, (socklen_t *)&slen) == -1){
+            terminar("recvfrom()");
+        }
+        //printa os dados recebidos
+        printf("%s\n",buf);
+    }
 
-    		nBytes = strlen(buffer) + 1;
-    
-    		/*Envia a mensagem para o servidor*/
-    		sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+    close(s);
+}
 
-    		/*Recebe a mensagem do servidor*/
-        	nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
-
-    		printf("Mensagem recebida do servidor: %s\n",buffer);
-    		if((strcmp(buffer,"FIM")!=0)&&(strcmp(buffer,"fim")!=0)){
-    			printf("digite 'fim' caso queira encerrar a conexao");	
-    		}else{
-    			printf("servidor encerrou a conexao");
-    		}
-  		}
-	}
-
-#endif
-
-int main(){
-	StartCliente();	
-	return 0;
+int main()
+{
+    IniciarCliente();
+    return 0;
 }
---
+++ Server.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#define BUFFTAM 1024
+#define PORTA 8888
+
+void terminar(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void IniciarServer(){
+    struct sockaddr_in servidorsock, cliente;
+    int s, slen = sizeof(cliente);
+
+    char buf[BUFFTAM];
+
+    //cria um scoket udp
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+        terminar("socket");
+    }
+
+    // zera a estrutura
+    memset((char *) &servidorsock, 0, sizeof(servidorsock));
+
+    servidorsock.sin_family = AF_INET;
+    servidorsock.sin_port = htons(PORTA);
+    servidorsock.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    //faz o bind do socket com a porta
+    if( bind(s , (struct sockaddr*)&servidorsock, sizeof(servidorsock) ) == -1){
+        terminar("bind");
+    }
+
+    //espera por dados
+    while(strcmp(buf,"fim\n")!=0){
+        printf("Esperando por dados\n");
+        fflush(stdout);
+        memset(buf,'\0',BUFFTAM);
+
+        //tenta receber algum dado do cliente
+        if (recvfrom(s, buf, BUFFTAM, 0, (struct sockaddr *) &cliente, (socklen_t*)&slen) == -1){
+            terminar("recvfrom()");
+        }
+
+        //imprime os dados recebidos
+        printf("Origem do cliente %s:%d\n", inet_ntoa(cliente.sin_addr), ntohs(cliente.sin_port));
+        printf("mensagem: %s\n" , buf);
+
+        printf("Digite a mensagem que deseja enviar ao cliente : ");
+        fgets(buf,BUFFTAM,stdin);
+
+        //retorna o cliente com a mensagem
+        if (sendto(s, buf, strlen(buf), 0, (struct sockaddr*) &cliente, slen) == -1){
+            terminar("sendto()");
+        }
+    }
+
+}
+
+
+int main()
+{
+    IniciarServer();
+    return 0;
+}
